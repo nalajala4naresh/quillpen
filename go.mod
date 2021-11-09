c07ec54c7e94483be3d6b399ac50b1720af7e58f
nalajala4naresh <nalajala4naresh@gmail.com>
2021-11-09 07:04:47 -0800

    chore: added login form and signup forms

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module quillpen
 go 1.16
 
 require (
-	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/gorilla/sessions v1.2.1
+	go.mongodb.org/mongo-driver v1.7.4
 )
--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"text/template"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
+	"github.com/gorilla/sessions"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 //go:embed index.html
 var Templates embed.FS
+
+
+var cookie_store = sessions.NewCookieStore([]byte("NareshPass"))
+
+type Qsession struct {
+
+	Id primitive.ObjectID `bson: "_id,omitempty"`
+
+}
 
 type IndexCard struct {
 	Title string
@@ -25,21 +45,69 @@ func ArticleHandler(resp http.ResponseWriter, req *http.Request) {
 }
 
 
-func main() {
 
-	router := mux.NewRouter()
-	router.Schemes("https")
-	router.Use()
+
+func main() {
+    
+
+	connect_context, cancel := context.WithTimeout(context.Background(), 3* time.Second)
+	defer cancel()
+
+	mongo_client, err := mongo.Connect(connect_context,options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		panic("Unable to Connect to mongo DB")
+	}
+	defer func(){
+		if err = mongo_client.Disconnect(connect_context); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := mongo_client.Ping(context.TODO(),readpref.Primary()); err != nil {
+		panic("Pinged the primary and Connected")
+	}
+
+	fmt.Println("Successfully connected and pinged.")
+
+	databases, derr := mongo_client.ListDatabaseNames(connect_context,bson.M{})
+    
+	if derr != nil {
+		println(derr.Error())
+	}
+
+	fmt.Println(databases)
+
+	quickstartDb := mongo_client.Database("quickstart")
+	podcasts := quickstartDb.Collection("podcasts")
+	result, err := podcasts.InsertOne(connect_context,bson.D{{"Name","Naresh"},{"age",25}})
+	if err != nil {
+		log.Fatal("Unable to insert the data")
+
+	}
+	println(result.InsertedID)
+
+
+
+
+	
 	
 
 
+
+
+
+	router := mux.NewRouter()
+	router.Schemes("https")
+
+
 	router.HandleFunc("/",IndexHandler).Methods("GET")
-	router.HandleFunc("/login",LoginHandler).Methods("POST")
+	router.HandleFunc("/signup",SignUpHandler).Methods("POST")
 
 	router.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
     
 	logged_handlers := handlers.LoggingHandler(os.Stdout,router)
-	contetTypeHandler := handlers.ContentTypeHandler(logged_handlers,"application/json")
+	contetTypeHandler := handlers.ContentTypeHandler(logged_handlers,"application/json","application/x-www-form-urlencoded")
     compressedHandlers := handlers.CompressHandler(contetTypeHandler)
 	http.ListenAndServe(":8080",compressedHandlers)
 
@@ -49,13 +117,53 @@ func main() {
 }
 
 func IndexHandler(resp http.ResponseWriter, req *http.Request) {
- mess := string("Hi form naresh")
- resp.Write([]byte(mess))
+	session, _ := cookie_store.Get(req, "_first")
+
+	session.Values["name"] = "naresh"
+	session.Values["age"]= 28
+	err := session.Save(req, resp)
+	if err != nil {
+			http.Error(resp, err.Error(), http.StatusInternalServerError)
+			return
+		}
+ 	
+	index := template.Must(template.ParseFS(Templates, "index.html"))
+	index.Execute(resp,nil)
 
 
 }
 
-func LoginHandler(resp http.ResponseWriter, req *http.Request) {
+type SignUpForm struct {
+
+	FirstName string
+	LastName string
+	Phone string
+	Email string
+	Password string
+
+}
+
+func SignUpHandler(resp http.ResponseWriter, req *http.Request) {
+
+
+	err := req.ParseForm()
+	if err != nil {
+    //    Invalid form data
+
+	}
+    var account_info SignUpForm
+	decoder:= schema.NewDecoder()
+	decoder.Decode(&account_info,req.Form)
+
+	fmt.Println("Got the login deets as follows %s, %s, %s",account_info.Phone, account_info.Email,
+account_info.Password)
+
+
+
+}
+
+func readDocuments() {
+
 
 
 }
